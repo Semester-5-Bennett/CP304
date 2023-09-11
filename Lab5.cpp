571f19f4d2bbc17f6bd2ab680c723edf535f37e2
Vikesh Laharpure <112697983+Vikesh8107@users.noreply.github.com>
2023-09-11 16:24:29 +0530

    Lab5

--- Lab5.cpp
+++ Lab5.cpp
@@ -7,6 +7,8 @@ int main() {
     int n;
     cin >> n;
 
+    // defining the two vector
+    // line1 & line2
     vector<int> line1(n), line2(n);
     for (int i = 0; i < n; i++) {
         cin >> line1[i];
@@ -15,13 +17,17 @@ int main() {
         cin >> line2[i];
     }
 
+    // defining the 2D dynamic vector
     vector<vector<int>> dp(n, vector<int>(2));
 
+    // for searching in the line 1 & line 2
     dp[0][0] = line1[0];
     dp[0][1] = line2[0];
 
+    // defining the function
     int maxTotalHeight = max(dp[0][0], dp[0][1]);
 
+    // finding the maximum height
     for (int i = 1; i < n; i++) {
         dp[i][0] = max(dp[i - 1][1] + line1[i], dp[i - 1][0]);
         dp[i][1] = max(dp[i - 1][0] + line2[i], dp[i - 1][1]);
